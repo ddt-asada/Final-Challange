db3106c013c1a055b703e8c384808f35850ec347
ddt-asada <ddt.asada@gmail.com>
2017-09-20 19:34:56 +0900

    最終課題 メイン画面へのコントロールの配置が完了 クリックイベント登録はまだ

--- JSONGUI/JSONGUI/JSONGUI.h
+++ JSONGUI/JSONGUI/JSONGUI.h
@@ -12,6 +12,9 @@ namespace JSONGUI {
 	/// <summary>
 	/// JSONGUI の概要
 	/// </summary>
+	/*概要：JSONを視覚的に作成するためのGUIメイン画面
+	作成日：2017.9.20
+	作成者：K.Asada*/
 	public ref class JSONGUI : public System::Windows::Forms::Form
 	{
 	public:
@@ -34,6 +37,37 @@ namespace JSONGUI {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TabControl^  tabControl1;
+	protected:
+	private: System::Windows::Forms::TabPage^  tabPage;
+	private: System::Windows::Forms::Button^  buttonTableCancel;
+	private: System::Windows::Forms::Button^  buttonTableOK;
+	private: System::Windows::Forms::Button^  buttonConnect;
+	private: System::Windows::Forms::Button^  buttonOption;
+	private: System::Windows::Forms::TextBox^  textBoxRow;
+	private: System::Windows::Forms::TextBox^  textBoxCol;
+	private: System::Windows::Forms::TextBox^  textBoxCell;
+	private: System::Windows::Forms::PictureBox^  pictureBoxCurrent;
+	private: System::Windows::Forms::PictureBox^  pictureBoxTable;
+	private: System::Windows::Forms::TabPage^  tabPage2;
+	private: System::Windows::Forms::Button^  buttonTableConv;
+	private: System::Windows::Forms::Button^  buttonListConv;
+
+
+
+	private: System::Windows::Forms::Button^  buttonListCancel;
+
+	private: System::Windows::Forms::Button^  buttonListOK;
+
+	private: System::Windows::Forms::Button^  buttonListConnect;
+
+	private: System::Windows::Forms::Button^  buttonListOption;
+
+	private: System::Windows::Forms::TextBox^  textBoxListRow;
+	private: System::Windows::Forms::TextBox^  textBoxList;
+	private: System::Windows::Forms::PictureBox^  pictureBoxListCurr;
+	private: System::Windows::Forms::PictureBox^  pictureBoxList;
+	private: System::Windows::Forms::Button^  buttonJoin;
 
 	private:
 		/// <summary>
@@ -48,12 +82,285 @@ namespace JSONGUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"JSONGUI";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage = (gcnew System::Windows::Forms::TabPage());
+			this->buttonTableConv = (gcnew System::Windows::Forms::Button());
+			this->buttonTableCancel = (gcnew System::Windows::Forms::Button());
+			this->buttonTableOK = (gcnew System::Windows::Forms::Button());
+			this->buttonConnect = (gcnew System::Windows::Forms::Button());
+			this->buttonOption = (gcnew System::Windows::Forms::Button());
+			this->textBoxRow = (gcnew System::Windows::Forms::TextBox());
+			this->textBoxCol = (gcnew System::Windows::Forms::TextBox());
+			this->textBoxCell = (gcnew System::Windows::Forms::TextBox());
+			this->pictureBoxCurrent = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBoxTable = (gcnew System::Windows::Forms::PictureBox());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->buttonListConv = (gcnew System::Windows::Forms::Button());
+			this->buttonListCancel = (gcnew System::Windows::Forms::Button());
+			this->buttonListOK = (gcnew System::Windows::Forms::Button());
+			this->buttonListConnect = (gcnew System::Windows::Forms::Button());
+			this->buttonListOption = (gcnew System::Windows::Forms::Button());
+			this->textBoxListRow = (gcnew System::Windows::Forms::TextBox());
+			this->textBoxList = (gcnew System::Windows::Forms::TextBox());
+			this->pictureBoxListCurr = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBoxList = (gcnew System::Windows::Forms::PictureBox());
+			this->buttonJoin = (gcnew System::Windows::Forms::Button());
+			this->tabControl1->SuspendLayout();
+			this->tabPage->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxCurrent))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxTable))->BeginInit();
+			this->tabPage2->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxListCurr))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxList))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Controls->Add(this->tabPage);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->tabControl1->Location = System::Drawing::Point(0, 0);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(1460, 869);
+			this->tabControl1->TabIndex = 0;
+			// 
+			// tabPage
+			// 
+			this->tabPage->AutoScroll = true;
+			this->tabPage->Controls->Add(this->buttonJoin);
+			this->tabPage->Controls->Add(this->buttonTableConv);
+			this->tabPage->Controls->Add(this->buttonTableCancel);
+			this->tabPage->Controls->Add(this->buttonTableOK);
+			this->tabPage->Controls->Add(this->buttonConnect);
+			this->tabPage->Controls->Add(this->buttonOption);
+			this->tabPage->Controls->Add(this->textBoxRow);
+			this->tabPage->Controls->Add(this->textBoxCol);
+			this->tabPage->Controls->Add(this->textBoxCell);
+			this->tabPage->Controls->Add(this->pictureBoxCurrent);
+			this->tabPage->Controls->Add(this->pictureBoxTable);
+			this->tabPage->Location = System::Drawing::Point(8, 39);
+			this->tabPage->Name = L"tabPage";
+			this->tabPage->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage->Size = System::Drawing::Size(1444, 822);
+			this->tabPage->TabIndex = 0;
+			this->tabPage->Text = L"テーブル";
+			this->tabPage->UseVisualStyleBackColor = true;
+			// 
+			// buttonTableConv
+			// 
+			this->buttonTableConv->Location = System::Drawing::Point(1292, 6);
+			this->buttonTableConv->Name = L"buttonTableConv";
+			this->buttonTableConv->Size = System::Drawing::Size(146, 68);
+			this->buttonTableConv->TabIndex = 9;
+			this->buttonTableConv->Text = L"変換";
+			this->buttonTableConv->UseVisualStyleBackColor = true;
+			// 
+			// buttonTableCancel
+			// 
+			this->buttonTableCancel->Location = System::Drawing::Point(1140, 6);
+			this->buttonTableCancel->Name = L"buttonTableCancel";
+			this->buttonTableCancel->Size = System::Drawing::Size(146, 68);
+			this->buttonTableCancel->TabIndex = 8;
+			this->buttonTableCancel->Text = L"キャンセル";
+			this->buttonTableCancel->UseVisualStyleBackColor = true;
+			// 
+			// buttonTableOK
+			// 
+			this->buttonTableOK->Location = System::Drawing::Point(988, 6);
+			this->buttonTableOK->Name = L"buttonTableOK";
+			this->buttonTableOK->Size = System::Drawing::Size(146, 68);
+			this->buttonTableOK->TabIndex = 7;
+			this->buttonTableOK->Text = L"OK";
+			this->buttonTableOK->UseVisualStyleBackColor = true;
+			// 
+			// buttonConnect
+			// 
+			this->buttonConnect->Location = System::Drawing::Point(844, 6);
+			this->buttonConnect->Name = L"buttonConnect";
+			this->buttonConnect->Size = System::Drawing::Size(138, 68);
+			this->buttonConnect->TabIndex = 6;
+			this->buttonConnect->Text = L"通信";
+			this->buttonConnect->UseVisualStyleBackColor = true;
+			// 
+			// buttonOption
+			// 
+			this->buttonOption->Location = System::Drawing::Point(680, 6);
+			this->buttonOption->Name = L"buttonOption";
+			this->buttonOption->Size = System::Drawing::Size(158, 68);
+			this->buttonOption->TabIndex = 5;
+			this->buttonOption->Text = L"設定";
+			this->buttonOption->UseVisualStyleBackColor = true;
+			// 
+			// textBoxRow
+			// 
+			this->textBoxRow->Location = System::Drawing::Point(124, 6);
+			this->textBoxRow->Name = L"textBoxRow";
+			this->textBoxRow->Size = System::Drawing::Size(100, 31);
+			this->textBoxRow->TabIndex = 4;
+			// 
+			// textBoxCol
+			// 
+			this->textBoxCol->Location = System::Drawing::Point(124, 43);
+			this->textBoxCol->Name = L"textBoxCol";
+			this->textBoxCol->Size = System::Drawing::Size(100, 31);
+			this->textBoxCol->TabIndex = 3;
+			// 
+			// textBoxCell
+			// 
+			this->textBoxCell->Location = System::Drawing::Point(1246, 316);
+			this->textBoxCell->Multiline = true;
+			this->textBoxCell->Name = L"textBoxCell";
+			this->textBoxCell->Size = System::Drawing::Size(140, 76);
+			this->textBoxCell->TabIndex = 2;
+			// 
+			// pictureBoxCurrent
+			// 
+			this->pictureBoxCurrent->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->pictureBoxCurrent->Location = System::Drawing::Point(1254, 425);
+			this->pictureBoxCurrent->Name = L"pictureBoxCurrent";
+			this->pictureBoxCurrent->Size = System::Drawing::Size(83, 102);
+			this->pictureBoxCurrent->TabIndex = 1;
+			this->pictureBoxCurrent->TabStop = false;
+			// 
+			// pictureBoxTable
+			// 
+			this->pictureBoxTable->Location = System::Drawing::Point(38, 377);
+			this->pictureBoxTable->Name = L"pictureBoxTable";
+			this->pictureBoxTable->Size = System::Drawing::Size(647, 424);
+			this->pictureBoxTable->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->pictureBoxTable->TabIndex = 0;
+			this->pictureBoxTable->TabStop = false;
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Controls->Add(this->buttonListConv);
+			this->tabPage2->Controls->Add(this->buttonListCancel);
+			this->tabPage2->Controls->Add(this->buttonListOK);
+			this->tabPage2->Controls->Add(this->buttonListConnect);
+			this->tabPage2->Controls->Add(this->buttonListOption);
+			this->tabPage2->Controls->Add(this->textBoxListRow);
+			this->tabPage2->Controls->Add(this->textBoxList);
+			this->tabPage2->Controls->Add(this->pictureBoxListCurr);
+			this->tabPage2->Controls->Add(this->pictureBoxList);
+			this->tabPage2->Location = System::Drawing::Point(8, 39);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(1444, 822);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"リスト";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// buttonListConv
+			// 
+			this->buttonListConv->Location = System::Drawing::Point(1276, 14);
+			this->buttonListConv->Name = L"buttonListConv";
+			this->buttonListConv->Size = System::Drawing::Size(146, 68);
+			this->buttonListConv->TabIndex = 19;
+			this->buttonListConv->Text = L"変換";
+			this->buttonListConv->UseVisualStyleBackColor = true;
+			// 
+			// buttonListCancel
+			// 
+			this->buttonListCancel->Location = System::Drawing::Point(1124, 14);
+			this->buttonListCancel->Name = L"buttonListCancel";
+			this->buttonListCancel->Size = System::Drawing::Size(146, 68);
+			this->buttonListCancel->TabIndex = 18;
+			this->buttonListCancel->Text = L"キャンセル";
+			this->buttonListCancel->UseVisualStyleBackColor = true;
+			// 
+			// buttonListOK
+			// 
+			this->buttonListOK->Location = System::Drawing::Point(972, 14);
+			this->buttonListOK->Name = L"buttonListOK";
+			this->buttonListOK->Size = System::Drawing::Size(146, 68);
+			this->buttonListOK->TabIndex = 17;
+			this->buttonListOK->Text = L"OK";
+			this->buttonListOK->UseVisualStyleBackColor = true;
+			// 
+			// buttonListConnect
+			// 
+			this->buttonListConnect->Location = System::Drawing::Point(828, 14);
+			this->buttonListConnect->Name = L"buttonListConnect";
+			this->buttonListConnect->Size = System::Drawing::Size(138, 68);
+			this->buttonListConnect->TabIndex = 16;
+			this->buttonListConnect->Text = L"通信";
+			this->buttonListConnect->UseVisualStyleBackColor = true;
+			// 
+			// buttonListOption
+			// 
+			this->buttonListOption->Location = System::Drawing::Point(664, 14);
+			this->buttonListOption->Name = L"buttonListOption";
+			this->buttonListOption->Size = System::Drawing::Size(158, 68);
+			this->buttonListOption->TabIndex = 15;
+			this->buttonListOption->Text = L"設定";
+			this->buttonListOption->UseVisualStyleBackColor = true;
+			// 
+			// textBoxListRow
+			// 
+			this->textBoxListRow->Location = System::Drawing::Point(94, 14);
+			this->textBoxListRow->Name = L"textBoxListRow";
+			this->textBoxListRow->Size = System::Drawing::Size(100, 31);
+			this->textBoxListRow->TabIndex = 14;
+			// 
+			// textBoxList
+			// 
+			this->textBoxList->Location = System::Drawing::Point(1230, 324);
+			this->textBoxList->Multiline = true;
+			this->textBoxList->Name = L"textBoxList";
+			this->textBoxList->Size = System::Drawing::Size(140, 76);
+			this->textBoxList->TabIndex = 13;
+			// 
+			// pictureBoxListCurr
+			// 
+			this->pictureBoxListCurr->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->pictureBoxListCurr->Location = System::Drawing::Point(1238, 433);
+			this->pictureBoxListCurr->Name = L"pictureBoxListCurr";
+			this->pictureBoxListCurr->Size = System::Drawing::Size(83, 102);
+			this->pictureBoxListCurr->TabIndex = 12;
+			this->pictureBoxListCurr->TabStop = false;
+			// 
+			// pictureBoxList
+			// 
+			this->pictureBoxList->Location = System::Drawing::Point(22, 385);
+			this->pictureBoxList->Name = L"pictureBoxList";
+			this->pictureBoxList->Size = System::Drawing::Size(647, 424);
+			this->pictureBoxList->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->pictureBoxList->TabIndex = 11;
+			this->pictureBoxList->TabStop = false;
+			// 
+			// buttonJoin
+			// 
+			this->buttonJoin->Location = System::Drawing::Point(507, 6);
+			this->buttonJoin->Name = L"buttonJoin";
+			this->buttonJoin->Size = System::Drawing::Size(167, 68);
+			this->buttonJoin->TabIndex = 10;
+			this->buttonJoin->Text = L"結合/解除";
+			this->buttonJoin->UseVisualStyleBackColor = true;
+			// 
+			// JSONGUI
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(13, 24);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoScroll = true;
+			this->ClientSize = System::Drawing::Size(1460, 869);
+			this->Controls->Add(this->tabControl1);
+			this->Name = L"JSONGUI";
+			this->Text = L"JSONGUI";
+			this->tabControl1->ResumeLayout(false);
+			this->tabPage->ResumeLayout(false);
+			this->tabPage->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxCurrent))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxTable))->EndInit();
+			this->tabPage2->ResumeLayout(false);
+			this->tabPage2->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxListCurr))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxList))->EndInit();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+};
 }
